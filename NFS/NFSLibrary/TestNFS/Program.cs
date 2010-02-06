2fbd4fae36e286f3db38299493215688019ef876
mirko.gatto <mirko.gatto@e2ca33bc-fee2-11de-9748-a16e6a44286a>
2010-02-06 16:01:29 +0000

    added c# wrapper added c# test application

---
+++ NFS/NFSLibrary/TestNFS/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NekoDrive.NFS.Wrappers;
+using System.Net;
+
+namespace TestNFS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress Address = IPAddress.Parse("192.168.56.102");
+            using (NFSv2 nfsv2 = new NFSv2())
+            {
+                if (nfsv2.Connect((UInt32) Address.Address) == NFSResult.NFS_SUCCESS)
+                {
+                    List<String> DevicesList = nfsv2.GetExportedDevices();
+                    if (DevicesList.Count > 0)
+                    {
+                        nfsv2.MountDevice(DevicesList[0]);
+                        List<String> ItemsList = nfsv2.GetItemList();
+                        foreach (String Item in ItemsList)
+                        {
+                            Console.WriteLine(Item);
+                        }
+                        nfsv2.UnMountDevice();
+                    }
+                    nfsv2.Disconnect();
+                }
+            }
+        }
+    }
+}
