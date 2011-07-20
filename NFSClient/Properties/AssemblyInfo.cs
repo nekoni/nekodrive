b6ee31bb3798445cc0d7bfd83dbadb0babaf0d97
mirko.gatto <mirko.gatto@e2ca33bc-fee2-11de-9748-a16e6a44286a>
2011-07-20 19:52:01 +0000

    version changes

--- NFSClient/Properties/AssemblyInfo.cs
+++ NFSClient/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.7.*")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
